28efa6e772530a6e0b19ab10e2286331ab6bb060
red rackhir <red rackhir@AsusPC.home>
2018-04-20 14:34:39 +0200

    Gilded-rose_kata

---
+++ Kata_refact_GildedRose/src/main/java/com/gildedrose/ArticuloBackstage.java
@@ -0,0 +1,31 @@
+package com.gildedrose;
+
+/**
+ *
+ * @author red rackhir
+ */
+public class ArticuloBackstage extends ArticuloComun implements Updateable {
+
+    public ArticuloBackstage(String name, int sellIn, int quality) {
+        super(name, sellIn, quality);
+    }
+
+    @Override
+    public void updateQuality() {
+        sellIn--;
+        if (sellIn > 10) {
+            quality++;
+            return;
+        }
+        if (sellIn < 0) {
+            quality = 0;
+            return;
+        }
+        if (sellIn < 10) {
+            quality += 2;
+        }
+        if (sellIn < 5) {
+            quality++;
+        }
+    }
+}
---
+++ Kata_refact_GildedRose/src/main/java/com/gildedrose/ArticuloBrie.java
@@ -0,0 +1,26 @@
+package com.gildedrose;
+
+/**
+ *
+ * @author red rackhir
+ */
+public class ArticuloBrie extends ArticuloComun implements Updateable {
+
+    public ArticuloBrie(String name, int sellIn, int quality) {
+        super(name, sellIn, quality);
+    }
+
+    @Override
+    public void updateQuality() {
+        sellIn--;
+        if (sellIn >= 0) {
+            quality++;
+        } else {
+            quality += 2;
+        }
+        if (quality > 50) {
+            quality = 50;
+        }
+    }
+
+}
