f3ac35a8b7cf085fec80f5c0053898f4ec937c4f
soib1a20 <soib1a20@ORDA21018.mshome.net>
2018-04-24 20:29:25 +0200

    Funcional 100%

---
+++ Kata_GildedRose/test/kata_gildedrose/GildedRoseTest.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kata_gildedrose;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author soib1a20
+ */
+public class GildedRoseTest {
+    
+    public GildedRoseTest() {
+    }
+
+    @Test
+    public void testUpdateQuality() {
+        System.out.println("updateQuality");
+        GildedRose instance = null;
+        instance.updateQuality();
+        fail("The test case is a prototype.");
+    }
+    
+}
